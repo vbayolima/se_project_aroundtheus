f4a72fc6bef53816703871ee4b1e222c568edd77
Valentina Bayolima <valentinabayolima@gmail.com>
2023-06-23 01:20:16 -0400

    added functionality to edit profile modal in js

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,19 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+/* Elements */
+const profileEditBtn = document.querySelector("#profile__edit-button");
+const profileEditModal = document.querySelector("#profile__edit-modal");
+
+const modalCloseBtn = document.querySelector("#modal__close-button");
+
+/* Shows modal to screen by adding modal_opened modifier from css */
+profileEditBtn.addEventListener("click", function () {
+  profileEditModal.classList.add("modal_opened");
+});
+
+/* Removes modal_opened modifier when click close button of modal */
+modalCloseBtn.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_opened");
+});
